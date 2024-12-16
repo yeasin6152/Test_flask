8c36318d7b3be35cb3e9311ebf1f5ac25ebb3b31
yeasin <37853204+yeasin6152@users.noreply.github.com>
2024-12-17 01:42:09 +0600

    Update my_flask.py

--- my_flask.py
+++ my_flask.py
@@ -188,7 +188,10 @@ def allLink():
             'noplaylist': True,
             'skip_download': True,  # don't download, just get info
             'http_headers': {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+                'Accept-Language': 'en-US,en;q=0.9',
+                'Accept': '*/*',
+                'Connection': 'keep-alive'
             }
         }       
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
