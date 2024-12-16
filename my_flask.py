209021e20073f6431b755ed60d67eff33880d1f6
yeasin <37853204+yeasin6152@users.noreply.github.com>
2024-12-17 01:31:56 +0600

    Update my_flask.py

--- my_flask.py
+++ my_flask.py
@@ -187,6 +187,9 @@ def allLink():
             'quiet': True,
             'noplaylist': True,
             'skip_download': True,  # don't download, just get info
+            'http_headers': {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+            }
         }       
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
             info = ydl.extract_info(url, download=False)
